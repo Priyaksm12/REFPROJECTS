bab28ada7f386dc7f90f197a10c225c0cac5f069
Priyaksm12 <priyankaks.priya@gmail.com>
2022-03-10 20:25:33 +0530

    Uc7 added - Duplicate Record

---
+++ src/Day24/AddressBookMain.java
@@ -0,0 +1,18 @@
+package Day24;
+
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    public static Scanner scannerObject = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("---------- Welcome To Address Book Program!! ----------");
+        AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+        addressBookDirectory.operationDirectory();
+
+    }
+
+}
